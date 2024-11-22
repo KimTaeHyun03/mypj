c4c9b90b8e152e70c4cf07a70203031b517167d5
KimTaeHyun03 <rlaxogus03@gmail.com>
2024-11-22 21:01:51 +0900

    nav바 기본틀 잡기

---
+++ src/pages/login.js
@@ -0,0 +1,14 @@
+let Login = ()=>{
+				
+				return(
+				
+				<>
+				
+				<span>로그인창임</span>
+				
+				</>
+				
+				
+				);
+}
+export default Login;
---
+++ src/pages/main.js
@@ -0,0 +1,16 @@
+let Main = ()=>{
+				
+				return(
+				<>
+				
+				
+				<span>메인창임</span>
+				
+				
+				
+				</>
+				)
+				
+				
+}
+export default Main;
